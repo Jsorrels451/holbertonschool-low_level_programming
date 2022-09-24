7141ba64a85195573d3c5472d306da59df112130
jsorrels451 <jasonsorrels@msn.com>
2022-09-24 09:49:00 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - Entry point
+* _isdigit - Checks for a digit.
+* @c: An input character
+* Return: 1 If character is a digit.
+*/
+int isdigit(int c);
+int main(void)
+{
+int c;
+if (c >= '0', c <= '9', c++)
+{
+return (1);
+}
+if (c != '0', c != '9', c++)
+{
+return (0);
+}
+}
