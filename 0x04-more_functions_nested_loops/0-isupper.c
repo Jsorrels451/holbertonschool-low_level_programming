1747c2d41dbb4f24d495200b3255d40620a11a1b
jsorrels451 <jasonsorrels@msn.com>
2022-09-24 08:35:00 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,11 +7,11 @@
 int _isupper(int c);
 int main(void)
 {
-  char c;
-  if (c >= 65 && c <= 90)
-    {
-      return (1);
-    }
-  else
-    return (0);
+char c;
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+return (0);
 }
