118eed64f15dee7e8e24e9057f922aea405b8900
jsorrels451 <jasonsorrels@msn.com>
2022-09-26 10:27:27 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,9 +5,9 @@
 * @c: An input character.
 * Return: 1 If character is uppercase.
 */
-int _isupper(int c);
+int _isupper(int c)
 {
-if ((c >= 'A') && (c <= 'Z'))
+if (c >= 'A' && c <= 'Z')
 return (1);
 else
 return (0);
