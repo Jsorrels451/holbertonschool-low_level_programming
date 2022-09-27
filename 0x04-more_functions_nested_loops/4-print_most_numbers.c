b1146266c7b22b67a751dcc63ab7e4e330864250
jsorrels451 <jasonsorrels@msn.com>
2022-09-27 08:09:25 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,9 @@
 void print_most_numbers(void)
 {
 int n;
-for (n = '0'; n <= '9'; n++)
+for (; n <= '9'; n++)
 {
-if (n != '2' && n != '9')
+  if (n != '0' && n != '9')
 _putchar(n);
 }
 _putchar('\n');
